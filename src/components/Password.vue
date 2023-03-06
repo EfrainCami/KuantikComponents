0ce28260098ceb38eb4d1c22181490f632833f52
Efrain Arenas <efrain_escuela@outlook.com>
2023-03-05 21:45:32 -0600

    Imagen responsiva

--- src/components/Password.vue
+++ src/components/Password.vue
@@ -1,103 +1,115 @@
 <template>
-  <div class="flex flex-col items-center bg-white rounded-xl p-4 w-fit">
-    <div class="flex items-center m-6 space-x-4">
-      <img class="h-14" src="../assets/robot_saludo.de0acfb2.png" alt="" />
-      <h1 class="font-semibold text-4xl">Restablezca su contraseña</h1>
+  <div class="flex rounded-xl overflow-hidden bg-white">
+    <div class="w-1/2 hidden xl:block">
+      <img src="../assets/back.jpg" alt="" />
     </div>
-    <div class="flex space-x-3">
-      <input
-        v-model="formData.inputPasswordForm"
-        class="w-80 p-2 border-2 rounded-md border-green-400"
-        :type="passwordVisible"
-        name=""
-        id=""
-        placeholder="contraseña"
-        maxlength="20"
-      />
-      <img
-        class="cursor-pointer"
-        @click="showPassword"
-        :src="eyeIcon"
-        alt="Mostrar contraseña"
-      />
-    </div>
+    <div class="flex p-10 flex-col items-center justify-center">
+      <div class="flex items-center m-6 space-x-4">
+        <h1 class="font-semibold text-4xl">Restablezca su contraseña</h1>
+      </div>
+      <div class="flex space-x-3">
+        <input
+          v-model="formData.inputPasswordForm"
+          class="w-80 p-2 border-2 rounded-md border-green-400"
+          :type="passwordVisible"
+          name=""
+          id=""
+          placeholder="contraseña"
+          maxlength="20"
+        />
+        <img
+          class="cursor-pointer"
+          @click="showPassword"
+          :src="eyeIcon"
+          alt="Mostrar contraseña"
+        />
+      </div>
 
-    <div class="flex mt-2 items-center transition">
-      <p class="text-lg">Nivel de seguridad:</p>
-      <p :class="{'text-red-500': securityWeak, 'text-yellow-500': securityMedium, 'text-green-500': securityStrong}" class="text-lg transition">
-        {{ securityLevelDescription }}
-      </p>
-      <svg
-      class="transition"
-        :class="{'fill-red-500': securityWeak, 'fill-yellow-500': securityMedium, 'fill-green-500': securityStrong}"
-        height="24"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg"
+      <div class="flex mt-2 items-center transition">
+        <p class="text-lg">Nivel de seguridad:</p>
+        <p
+          :class="{
+            'text-red-500': securityWeak,
+            'text-yellow-500': securityMedium,
+            'text-green-500': securityStrong,
+          }"
+          class="text-lg transition"
+        >
+          {{ securityLevelDescription }}
+        </p>
+        <svg
+          class="transition"
+          :class="{
+            'fill-red-500': securityWeak,
+            'fill-yellow-500': securityMedium,
+            'fill-green-500': securityStrong,
+          }"
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="m21 11c0 5.55-3.84 10.74-9 12-5.16-1.26-9-6.45-9-12v-6l9-4 9 4zm-9 10c3.75-1 7-5.46 7-9.78v-4.92l-7-3.12-7 3.12v4.92c0 4.32 3.25 8.78 7 9.78m2.8-10v-1.5c0-1.4-1.4-2.5-2.8-2.5s-2.8 1.1-2.8 2.5v1.5c-.6 0-1.2.6-1.2 1.2v3.5c0 .7.6 1.3 1.2 1.3h5.5c.7 0 1.3-.6 1.3-1.2v-3.5c0-.7-.6-1.3-1.2-1.3m-1.3 0h-3v-1.5c0-.8.7-1.3 1.5-1.3s1.5.5 1.5 1.3z"
+          />
+        </svg>
+      </div>
+      <p class="mt-4 text-xl">La contraseña debe contener:</p>
+      <div class="flex flex-col items-start text-xl">
+        <div class="flex justify-center items-center">
+          <img
+            :class="{ hidden: !checkEightCharacters }"
+            class="h-4"
+            src="../assets/heavy-check-mark.svg"
+            alt=""
+          />
+          <p>8 caracteres como mínimo</p>
+        </div>
+        <div class="flex justify-center items-center">
+          <img
+            :class="{ hidden: !checkLowerCase }"
+            class="h-4"
+            src="../assets/heavy-check-mark.svg"
+            alt=""
+          />
+          <p>Minúsculas</p>
+        </div>
+        <div class="flex justify-center items-center">
+          <img
+            :class="{ hidden: !checkUpperCase }"
+            class="h-4"
+            src="../assets/heavy-check-mark.svg"
+            alt=""
+          />
+          <p>Mayúsculas</p>
+        </div>
+        <div class="flex justify-center items-center">
+          <img
+            :class="{ hidden: !checkSpecialCharacters }"
+            class="h-4"
+            src="../assets/heavy-check-mark.svg"
+            alt=""
+          />
+          <p>Símbolos (!$#%)</p>
+        </div>
+        <div class="flex justify-center items-center">
+          <img
+            :class="{ hidden: !checkNumbers }"
+            class="h-4"
+            src="../assets/heavy-check-mark.svg"
+            alt=""
+          />
+          <p>Números</p>
+        </div>
+      </div>
+      <button
+        @click="submit"
+        class="flex my-12 p-3 px-5 rounded-full text-white bg-blue-900"
       >
-        <path
-          d="m21 11c0 5.55-3.84 10.74-9 12-5.16-1.26-9-6.45-9-12v-6l9-4 9 4zm-9 10c3.75-1 7-5.46 7-9.78v-4.92l-7-3.12-7 3.12v4.92c0 4.32 3.25 8.78 7 9.78m2.8-10v-1.5c0-1.4-1.4-2.5-2.8-2.5s-2.8 1.1-2.8 2.5v1.5c-.6 0-1.2.6-1.2 1.2v3.5c0 .7.6 1.3 1.2 1.3h5.5c.7 0 1.3-.6 1.3-1.2v-3.5c0-.7-.6-1.3-1.2-1.3m-1.3 0h-3v-1.5c0-.8.7-1.3 1.5-1.3s1.5.5 1.5 1.3z"
-        />
-      </svg>
+        <img class="invert mr-3" src="../assets/arrow-right-1.svg" alt="" />
+        Restablecer contraseña
+      </button>
     </div>
-    <p class="mt-4 text-xl">La contraseña debe contener:</p>
-    <div class="flex flex-col items-start text-xl">
-      <div class="flex justify-center items-center">
-        <img
-          :class="{ hidden: !checkEightCharacters }"
-          class="h-4"
-          src="../assets/heavy-check-mark.svg"
-          alt=""
-        />
-        <p>8 caracteres como mínimo</p>
-      </div>
-      <div class="flex justify-center items-center">
-        <img
-          :class="{ hidden: !checkLowerCase }"
-          class="h-4"
-          src="../assets/heavy-check-mark.svg"
-          alt=""
-        />
-        <p>Minúsculas</p>
-      </div>
-      <div class="flex justify-center items-center">
-        <img
-          :class="{ hidden: !checkUpperCase }"
-          class="h-4"
-          src="../assets/heavy-check-mark.svg"
-          alt=""
-        />
-        <p>Mayúsculas</p>
-      </div>
-      <div class="flex justify-center items-center">
-        <img
-          :class="{ hidden: !checkSpecialCharacters }"
-          class="h-4"
-          src="../assets/heavy-check-mark.svg"
-          alt=""
-        />
-        <p>Símbolos (!$#%)</p>
-      </div>
-      <div class="flex justify-center items-center">
-        <img
-          :class="{ hidden: !checkNumbers }"
-          class="h-4"
-          src="../assets/heavy-check-mark.svg"
-          alt=""
-        />
-        <p>Números</p>
-      </div>
-    </div>
-    <div class="flex items-start">
-      <p class="text-red-600">*hola</p>
-    </div>
-    <button
-      @click="submit"
-      class="flex mb-6 w-fit p-5 bg-blue-900 rounded-full text-white"
-    >
-      <img class="invert mr-3" src="../assets/arrow-right-1.svg" alt="" />
-      Restablecer contraseña
-    </button>
   </div>
 </template>
 
@@ -177,9 +189,9 @@ const securityStrong = ref(false);
 const securityLevelDescription = ref("NULO");
 
 const checkSecurityLevel = (securityLevel) => {
-    securityWeak.value = false
-    securityMedium.value = false
-    securityStrong.value = false
+  securityWeak.value = false;
+  securityMedium.value = false;
+  securityStrong.value = false;
   if (securityLevel < 3) {
     securityLevelDescription.value = "DÉBIL";
     securityWeak.value = true;
@@ -206,7 +218,7 @@ const rules = {
     hasSpecialCharacters,
   },
 };
- 
+
 const v$ = useVuelidate(rules, formData);
 
 onUpdated(() => {
